4e85c1908f8f99d57fdddd529ae05951ba7c4785
smita-dev <smitakpatil26@gmail.com>
2021-05-27 23:06:24 +0530

    added hooks for text and amount and onchange setting the value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Balance from "./Components/Balance";
+import IncomeExpense from "./Components/IncomeExpense";
+import TransactionList from "./Components/TransactionList";
+import AddTransaction from "./Components/AddTransaction";
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
   return (
     <>
       <h3>Add new transaction</h3>
       <form>
-        <div>
+        <div className="form-control">
           <label>Text</label>
-          <input type="text" placeholder="Enter Text.." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+              console.log(text);
+            }}
+            placeholder="Enter Text.."
+          />
         </div>
-        <div>
+        <div className="form-control">
           <label>
             Amount <br />
             (negative -expense, positive-income)
           </label>
-          <input type="number" placeholder="Enter Amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+              console.log(amount);
+            }}
+            placeholder="Enter Amount..."
+          />
         </div>
         <button className="btn">Add Transaction</button>
       </form>
